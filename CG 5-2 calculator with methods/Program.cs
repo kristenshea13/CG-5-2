2ac3ba274b44e91bafdf60ebf2fda5b1b7d02f7f
Kristen Shea <kstar61@live.com>
2018-08-07 22:40:17 -0500

    **

--- CG 5-2 calculator with methods/Program.cs
+++ CG 5-2 calculator with methods/Program.cs
@@ -16,47 +16,59 @@ namespace CG_5_2_calculator_with_methods
             Console.WriteLine("Calculator");
             Console.WriteLine(); //blank line between name of app and first variable entry to make it easy to read
 
-            /*ask for first number, convert variable type from string to decimal (using this instead of int to 
-             * allow for decimal places in variable entries and answer. i skipped separate line of code to declare variable 
-             * as a string and just converted user entry to decimal all on one line of code*/
+            /*ask for first number, convert variable type from string to int 
+             * (using int in this app, should work for other number types as well.
+             * i skipped separate line of code to declare variable 
+             * as a string and just converted user entry to int all on one line of code*/
 
 
             Console.Write("Enter first number: ");
-            decimal firstNumber = decimal.Parse(Console.ReadLine());
+            int firstNumber = int.Parse(Console.ReadLine());
 
             //do the same for second number entry as first to convert from string to decimal
             Console.Write("Enter second number: ");
-            decimal secondNumber = decimal.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
 
             Console.Write("Would you like to add, subtract, multiply, or divide these two numbers? Please choose one: ");
             string mathFunction = Console.ReadLine();
 
             if (mathFunction == "add")
-            {
-
-            }
+                Console.WriteLine($"Answer: {AddAnswer(firstNumber, secondNumber)}");
 
             else if (mathFunction == "subtract")
-            {
+                Console.WriteLine($"Answer: {SubtractAnswer(firstNumber, secondNumber)}");
 
-            }
+            else if (mathFunction == "multiply")
+                Console.WriteLine($"Answer: {MultiplyAnswer(firstNumber, secondNumber)}");
 
 
+            Console.ReadLine();
 
         }
 
-        private decimal AddAnswer(decimal firstNumber, decimal secondNumber)
+        private static int AddAnswer(int firstNumber, int secondNumber)
           {
-             decimal addAnswer = firstNumber + secondNumber;
-             //return 
+            int additionMath = firstNumber + secondNumber;
+            return additionMath;
           }
 
             
+        private static int SubtractAnswer(int firstNumber, int secondNumber)
+        {
+            int subtractionMath = firstNumber - secondNumber;
+            return subtractionMath;
+        }
 
+        private static int MultiplyAnswer(int firstNumber, int secondNumber)
+        {
+            int multiplyMath = firstNumber * secondNumber;
+            return multiplyMath;
+        }
 
+        private static int DivideAnswer(int firstNumber, int secondNumber)
+        {
 
-
-
+        }
 
     }
 }

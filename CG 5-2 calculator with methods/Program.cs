8333f7f9b35675681809fb00c1dc408f5ba9af5c
Kristen Shea <kstar61@live.com>
2018-08-08 00:08:59 -0500

    *

--- CG 5-2 calculator with methods/Program.cs
+++ CG 5-2 calculator with methods/Program.cs
@@ -6,13 +6,6 @@ namespace CG_5_2_calculator_with_methods
     {
         static void Main(string[] args)
         {
-
-            /*Remember our simple calculator program from Class 3? Update the application so each of 
-             * the math calculations (add, subtract, multiple, divide) is each in its own method.  
-             * The methods should return the value and the main method should print the value.  Make 
-             * sure your methods have XML comments for IntelliSense. 
-            Upload your program to a new repository called CG 5-2. */
-
             Console.WriteLine("Calculator");
             Console.WriteLine(); //blank line between name of app and first variable entry to make it easy to read
 
@@ -29,8 +22,11 @@ namespace CG_5_2_calculator_with_methods
             Console.Write("Enter second number: ");
             int secondNumber = int.Parse(Console.ReadLine());
 
+            //ask user what they would like to do with both numbers, name and declare as a string variable
             Console.Write("Would you like to add, subtract, multiply, or divide these two numbers? Please choose one: ");
             string mathFunction = Console.ReadLine();
+
+            //create exception for a divisor of 0 to print error message and exit program
 
             if (secondNumber == 0 && mathFunction == "divide")
             {
@@ -39,41 +35,72 @@ namespace CG_5_2_calculator_with_methods
                 return;
             }
 
-            
+            //create statements to print answer from correct method to console based on user input for mathFunction
+            else if (mathFunction == "add")
+                Console.WriteLine("Answer: {0}",
+                    AddAnswer(firstNumber, secondNumber));
 
+            else if (mathFunction == "subtract")
+                Console.WriteLine("Answer: {0}",
+                    SubtractAnswer(firstNumber, secondNumber));
 
-                Console.WriteLine("Answer: {0}", 
-                    AddAnswer(firstNumber, secondNumber), 
-                    SubtractAnswer(firstNumber, secondNumber), 
-                    MultiplyAnswer(firstNumber, secondNumber), 
+            else if (mathFunction == "multiply")
+                Console.WriteLine("Answer: {0}",
+                    MultiplyAnswer(firstNumber, secondNumber));
+
+            else if (mathFunction == "divide")
+                Console.WriteLine("Answer: {0}",
                     DivideAnswer(firstNumber, secondNumber));
-
 
             Console.ReadLine();
 
         }
 
+        /// <summary>
+        /// add two user input numbers together
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>the answer to two user input numbers added together</returns>
         private static int AddAnswer(int firstNumber, int secondNumber)
-          {
+          {///
             int additionMath = firstNumber + secondNumber;
             return additionMath;
           }
 
             
+        /// <summary>
+        /// subtract second user input number from first
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>the answer to first number minus second number</returns>
         private static int SubtractAnswer(int firstNumber, int secondNumber)
-        {
+        {///
             int subtractionMath = firstNumber - secondNumber;
             return subtractionMath;
         }
 
+        /// <summary>
+        /// multiply the two user input numbers together
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>answer to first number multiplied by second number</returns>
         private static int MultiplyAnswer(int firstNumber, int secondNumber)
-        {
+        {///
             int multiplyMath = firstNumber * secondNumber;
             return multiplyMath;
         }
 
+        /// <summary>
+        /// divide first user input number by second
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>answer to first number divided by second number</returns>
         private static int DivideAnswer(int firstNumber, int secondNumber)
-        {
+        {///
             int divideMath = firstNumber / secondNumber;
             return divideMath;
                  
